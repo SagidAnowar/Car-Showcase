cb3d1c02d218fc93a20b5b308cf92362821afe50
Sagid Anowar <sagidanowar@gmail.com>
2023-08-07 20:15:02 -0400

    updated page and types. added searchbar, searchmanafacturers and customfilter

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomBottonProps {
     handleClick?: MouseEventHandler<HTMLButtonElement>
     btnType?: "button" | "submit";
 }
+
+export interface SearchManufacturerProps {
+    manufacturer: string
+    setManufacturer: (manufacturer: string) => void;
+  }
